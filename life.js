72e07f0142da54378c7af34d8e9ae8603ae8667d
2sky <a2865048049@163.com>
2024-08-20 20:35:09 +0800

    Update life.js

--- life.js
+++ life.js
@@ -2,22 +2,22 @@ var chartDom = document.getElementById('life');
 var myChart = echarts.init(chartDom);
 var option;
 const list = [
-  { id: 1, name: 'life', parentId: null },
-  { id: 2, name: '安详&和谐的内在', parentId: 1},
-  { id: 3, name: '冥想&内观', parentId: 2},
-  { id: 4, name: '十日禅', parentId: 2},
-  { id: 5, name: '领会般若智', parentId: 2},
-  { id: 6, name: '认知世界的思维框架', parentId: 1 },
-  { id: 61, name: '大量读书', parentId: 6 },
-  { id: 611, name: '怎么读？', parentId: 61 },
-  { id: 612, name: '读什么？', parentId: 61 },
-  { id: 613, name: '正在读', parentId: 61 },
-  { id: 614, name: '已读', parentId: 61 },
-  { id: 62, name: '构建思维框架', parentId: 6 },
-   { id: 621, name: '毛泽东思想？', parentId: 62 },
-   { id: 622, name: '悉达多？', parentId: 62 },
-  { id: 63, name: '改变世界', parentId: 6 },
-  { id: 7, name: 'Root 2', parentId: 1 },
+  { id: 0, name: 'life', parentId: null },
+  { id: 1, name: '安详&和谐的内在', parentId: 0},
+  { id: 11, name: '冥想&内观', parentId: 1},
+  { id: 12, name: '十日禅', parentId: 1},
+  { id: 13, name: '领会般若智', parentId: 1},
+  { id: 2, name: '认知世界的思维框架', parentId: 0 },
+  { id: 21, name: '大量读书', parentId: 2 },
+  { id: 211, name: '怎么读？', parentId: 21 },
+  { id: 212, name: '读什么？', parentId: 21 },
+  { id: 213, name: '正在读', parentId: 21 },
+  { id: 214, name: '已读', parentId: 21 },
+  { id: 22, name: '构建思维框架', parentId: 2 },
+  { id: 221, name: '毛泽东思想？', parentId: 22 },
+  { id: 222, name: '悉达多？', parentId: 22 },
+  { id: 23, name: '改变世界', parentId: 2 },
+  { id: 3, name: 'Root 2', parentId: 0 },
 ];
 
 function buildTree(list) {
